e0d06ae7ec6ada962c10fed06e3414ff38d4939e
Lucas04-nhr <117806313+Lucas04-nhr@users.noreply.github.com>
2023-01-04 20:14:04 +0800

    Add files via upload

---
+++ 12-期末练习2/03/Prog1.cpp
@@ -0,0 +1,43 @@
+
+/*-------------------------------------------------------
+【程序设计】
+---------------------------------------------------------
+
+题目：有一个时钟类，包含数据成员hour（小时）、minute（分）和sec（秒），
+
+定义三个获取数据成员的成员函数和一个模拟秒表的函数成员go（每调用一次go走一秒），
+
+格式：
+        void go(void)
+            int getHour(void) 
+        int getMinute(void) 
+        int getSec(void) 
+
+
+输出结果见图：样张.JPG
+-------------------------------------------------------*/
+#include <iostream>
+using namespace std;
+
+/**********Program**********/
+
+
+/**********  End  **********/
+void display(Clock ck)
+{
+        cout<<ck.getHour()<<"点"<<ck.getMinute()<<"分"<<ck.getSec()<<"秒";
+}
+void main(void)
+{ 
+        int i;
+        int INTERVAL;    // 耗时，单位为秒
+        INTERVAL=rand();
+        INTERVAL=rand();
+        Clock  clock(9,55,0);
+        cout<<"起点="; display(clock); cout<<endl;
+        for(i=1;i<=INTERVAL;i++) clock.go();
+        cout<<"终点="; display(clock); cout<<endl;
+        cout<<"耗时"<<INTERVAL<<"秒"<<endl;
+}
+
+
